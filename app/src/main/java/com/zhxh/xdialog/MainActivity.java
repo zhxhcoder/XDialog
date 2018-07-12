8dd6be319f96e0bff3a01b577ca9b6eb18e38c11
zhxh <zhxhcoder@gmail.com>
2018-07-12 10:58:04 +0800

    展示

--- app/src/main/java/com/zhxh/xdialog/MainActivity.java
+++ app/src/main/java/com/zhxh/xdialog/MainActivity.java
@@ -1,8 +1,8 @@
 package com.zhxh.xdialog;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(v -> startActivity(new Intent(MainActivity.this,ToastActivity.class)));
+        findViewById(R.id.btnToast).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ToastActivity.class)));
+        findViewById(R.id.btnDialog).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, DialogActivity.class)));
     }
 }
