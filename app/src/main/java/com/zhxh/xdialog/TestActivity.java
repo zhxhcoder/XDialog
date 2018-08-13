e289072352e9ff755084ada4d060208b16a7ef44
zhxh <zhxhcoder@gmail.com>
2018-08-13 19:44:52 +0800

    reactivex

--- app/src/main/java/com/zhxh/xdialog/TestActivity.java
+++ app/src/main/java/com/zhxh/xdialog/TestActivity.java
@@ -1,16 +1,30 @@
 package com.zhxh.xdialog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.zhxh.xdialog.component.QuantDkMainDialog;
+
+import java.util.concurrent.TimeUnit;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class TestActivity extends AppCompatActivity {
 
     private int dialogType = 1;
     private Button btnRed;
+    private TextView tvTime;
+    private String TAG = "zhxh_debug";
+    private String LINE_SEPARATOR = "\n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,7 @@ public class TestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test);
 
         btnRed = findViewById(R.id.btnRed);
+        tvTime = findViewById(R.id.tvTime);
 
         btnRed.setText("对话框" + String.valueOf(dialogType));
 
@@ -30,5 +45,51 @@ public class TestActivity extends AppCompatActivity {
 
             dialogType = (dialogType++) % 3 + 1;
         });
+
+
+        getObservable().delay(5, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(getObserver());
+
+
     }
+
+    private Observer<String> getObserver() {
+        return new Observer<String>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, " onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(String value) {
+                tvTime.append(LINE_SEPARATOR);
+                tvTime.append(" onNext : value : " + value);
+                tvTime.append(LINE_SEPARATOR);
+                Log.d(TAG, " onNext : value : " + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                tvTime.append(" onError : " + e.getMessage());
+                tvTime.append(LINE_SEPARATOR);
+                Log.d(TAG, " onError : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                tvTime.append(LINE_SEPARATOR);
+                tvTime.append(" onComplete");
+                tvTime.append(LINE_SEPARATOR);
+                Log.d(TAG, " onComplete");
+            }
+        };
+    }
+
+    private Observable<String> getObservable() {
+        return Observable.just("haha");
+    }
+
 }
